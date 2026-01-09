da67dd6b21a5b42383f8af091129e1a638d15fef
indraanjay <indrakampus01@gmail.com>
2026-01-09 13:38:54 +0800

    sepersekian dari staff aman

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Staff\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('staff')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('staff.dashboard.index');
+    Route::get('/dashboard/{id}', [DashboardController::class, 'show'])->name('staff.dashboard.show');
+    Route::put('/pesanan/verifikasi/{id}', [DashboardController::class, 'verifikasi'])->name('pesanan.verifikasi');
+    Route::put('/pesanan/cancel/{id}', [DashboardController::class, 'cancel'])->name('pesanan.cancel');
+    Route::put('/staff/pesanan/proses/{id}', [DashboardController::class, 'proses'])->name('pesanan.proses');
+    Route::put('/staff/pesanan/selesai/{id}', [DashboardController::class, 'selesai'])->name('pesanan.selesai');
 });
