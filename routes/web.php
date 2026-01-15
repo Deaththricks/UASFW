3b8e98cac400e99f83cc7d82173106936cbd2c1c
Deaththricks <saityasadipta@gmail.com>
2026-01-15 11:52:13 +0800

    added some feature of customer but not dont yet hehe

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Staff\DashboardController;
+use App\Http\Controllers\Customer\CustomerDashboardController;
+use App\Http\Controllers\Customer\CustomerKatalogController;
+use App\Http\Controllers\Customer\CustomerHistoryController;
+use App\Http\Controllers\Customer\CustomerProfileController;
+
 
 Route::prefix('staff')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('staff.dashboard.index');
@@ -10,4 +15,9 @@ Route::prefix('staff')->group(function () {
     Route::put('/pesanan/cancel/{id}', [DashboardController::class, 'cancel'])->name('pesanan.cancel');
     Route::put('/staff/pesanan/proses/{id}', [DashboardController::class, 'proses'])->name('pesanan.proses');
     Route::put('/staff/pesanan/selesai/{id}', [DashboardController::class, 'selesai'])->name('pesanan.selesai');
+});
+
+Route::prefix('customer')->group(function () {
+    Route::get('/dashboard', [CustomerDashboardController::class, 'index']);
+    Route::get('/katalog', [CustomerDashboardController::class, 'katalog'])->name('katalog');
 });
